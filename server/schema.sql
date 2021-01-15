3329759d95f7330af0e2c332570c54e96c3c4eec
phucciTr <miabam26@gmail.com>
2021-01-14 18:30:12 -0800

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -4,6 +4,10 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(30) NOT NULL,
+  text VARCHAR(50) NOT NULL,
+  roomname VARCHAR(30) NOT NULL
 );
 
 /* Create other tables and define schemas for them here! */
