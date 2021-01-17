254ddacf408476618d9db9c82e313e02c4845993
phucciTr <miabam26@gmail.com>
2021-01-16 18:30:18 -0800

    Refacotored db to Sequalize

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,40 +3,79 @@ var models = require('../models');
 module.exports = {
 
   messages: {
+
     get: function (req, res) {
-      // go into database, get (all messages) from the messages table
-
-      models.messages.get((err, result) => {
-        // if (err) { throw err; }
-        // console.log('result = ', result);
-        res.status(200).json(result);
-      });
-    }, // a function which handles a get request for all messages
-
-    post: function (req, res) {
-      let data = req.body;
-
-      models.messages.post(data, (err, result) => {
-        if (err) { throw err; }
-        res.status(201).json(result);
-      });
-    } // a function which handles posting a message to the database
-  },
-
-  users: {
-    // Ditto as above
-    get: function (req, res) {
-
+      db.Message.findAll({include: [db.User]})
+        .then(function(messages) {
+          res.json(messages);
+        });
     },
 
     post: function (req, res) {
-
       let data = req.body;
+      // Find the record matching the specified criteria. If no such record exists, create one using the provided initial values. or, if you need to know whether a new record was created.
+      console.log('data:', data);
+      db.User.findOrCreate({where: {userName: req.body.userName}})
 
-      models.users.post(data, (err, result) => {
-        if (err) { throw err; }
-        res.status(201).json(result);
-      });
+        .spread(function(user, created) {
+          db.Message.create({
+            userid: user.get('id'),
+            userName: req.body.userName,
+            userMessage: req.body.userMessage,
+            roomName: req.body.roomName
+          }).then(function(message) {
+            res.sendStatus(201);
+          });
+        });
+    }
+
+    // get: function (req, res) {
+    //   // go into database, get (all messages) from the messages table
+
+    //   models.messages.get((err, result) => {
+    //     res.status(200).json(result);
+    //   });
+    // }, // a function which handles a get request for all messages
+
+    // post: function (req, res) {
+    //   let data = req.body;
+
+    //   models.messages.post(data, (err, result) => {
+    //     if (err) { throw err; }
+    //     res.status(201).json(result);
+    //   });
+    // } // a function which handles posting a message to the database
+  },
+
+  users: {
+    get: function (req, res) {
+      db.User.findAll()
+        .then(function(users) {
+          res.json(users);
+        });
+    },
+    post: function (req, res) {
+      db.User.findOrCreate({where: {userName: req.body.userName}})
+        // findOrCreate returns multiple resutls in an array
+        // use spread to assign the array to function arguments
+        .spread(function(user, created) {
+          res.sendStatus(created ? 201 : 200);
+        });
     }
   }
+  //   // Ditto as above
+  //   get: function (req, res) {
+
+  //   },
+
+  //   post: function (req, res) {
+
+  //     let data = req.body;
+
+  //     models.users.post(data, (err, result) => {
+  //       if (err) { throw err; }
+  //       res.status(201).json(result);
+  //     });
+  //   }
+  // }
 };
--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,56 @@
-var mysql = require('mysql');
+var { Sequelize, model, datatypes } = require('sequelize');
 
-// Create a database connection and export it from this file.
-// You will need to connect with the user "root", no password,
-// and to the database "chat".
-
-var dbConnection = mysql.createConnection({
-  user: 'root',
-  password: '',
-  database: 'chat'
+var db = new Sequelize('chat', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
 });
 
-module.exports = dbConnection;
+// var db = new Sequelize('chat', 'root', '', {
+//   host: 'http://127.0.0.1:3306/',
+//   dialect: 'mysql'
+// });
+
+
+
+// we define the models we need using js--we don't need a schema file!
+var User = db.define('User', {
+  userName: Sequelize.STRING
+});
+
+var Message = db.define('Message', {
+  userName: Sequelize.STRING,
+  userMessage: Sequelize.STRING,
+  roomName: Sequelize.STRING
+});
+
+
+// puts a UserId column on each Message instance
+// also gives us the `.setUser` method available
+// after creating a new instance of Message
+Message.belongsTo(User);
+// enables bi-directional associations between Users and Messages
+User.hasMany(Message);
+
+
+User.sync();
+Message.sync();
+// creates these tables in MySQL if they don't already exist. Pass in {force: true}
+// to drop any existing user and message tables and make new ones.
+
+exports.User = User;
+exports.Message = Message;
+
+
+// var mysql = require('mysql');
+
+// // Create a database connection and export it from this file.
+// // You will need to connect with the user "root", no password,
+// // and to the database "chat".
+
+// var dbConnection = mysql.createConnection({
+//   user: 'root',
+//   password: '',
+//   database: 'chat'
+// });
+
+// module.exports = dbConnection;
